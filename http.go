e1f3f4e59eb8821d01655e65e0c8f37e54097c48
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-02-11 01:26:22 -0500

    change github username

--- http.go
+++ http.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/spf13/cobra"
 
-	"github.com/tinfoilanalytics/verifier/client"
+	"github.com/tinfoilsh/verifier/client"
 )
 
 func secureClient() *client.SecureClient {
