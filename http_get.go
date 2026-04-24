d2f8fb905c0f8c789d5297fb3afa0307821fc68f
Tanya Verma <vermatanyax@gmail.com>
2026-04-23 19:35:20 -0700

    feat: support headers for http requests

--- http_get.go
+++ http_get.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -15,11 +14,17 @@ var httpGetCmd = &cobra.Command{
 	Use:   "get [url]",
 	Short: "HTTP GET request",
 	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		resp, err := secureClient().Get(args[0], nil)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		headers, err := parseRequestHeaders(requestHeaders)
 		if err != nil {
-			log.Fatal(err)
+			return err
+		}
+
+		resp, err := secureClient().Get(args[0], headers)
+		if err != nil {
+			return err
 		}
 		fmt.Println(string(resp.Body))
+		return nil
 	},
 }
